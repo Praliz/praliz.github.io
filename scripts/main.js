fdd2bf64d054082a6d81e67d3b52d13a1938466a
Praliz <149048121+Praliz@users.noreply.github.com>
2023-10-26 09:36:10 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/pickachu.png") {
+    myImage.setAttribute("src", "images/angry.png");
+  } else {
+    myImage.setAttribute("src", "images/pickachu.png");
+  }
+};
+
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `My first website welcome, ${storedName}`;
+  }
+
+
+  myButton.onclick = () => {
+    setUserName();
+  };
